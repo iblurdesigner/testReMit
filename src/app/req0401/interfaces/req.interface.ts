dfb8f4bd70bfe00f4053ddd821490b84a55dd76f
iblurdesigner <nevomen@gmail.com>
2025-07-08 11:41:30 -0500

    uuid para id unico en requisitos

--- src/app/req0401/interfaces/req.interface.ts
+++ src/app/req0401/interfaces/req.interface.ts
@@ -1,4 +1,5 @@
 export interface Req {
+  id?: string;
   name: string;
   code: string;
 }
--- src/app/req0401/services/req0401.service.ts
+++ src/app/req0401/services/req0401.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@angular/core';
+
 import { Req } from '../interfaces/req.interface';
+import { v4 as uuid } from 'uuid';
+
 
 @Injectable({providedIn: 'root'})
 export class Req0401Service {
   
   public requisites: Req[] = [
     {
+      id: uuid(),
       name: 'Requisito 01',
       code: '004-02-01'
     },
     {
+      id: uuid(),
       name: 'Requisito 02',
       code: '004-02-02'
     },
     {
+      id: uuid(),
       name: 'Requisito 03',
       code: '004-02-03'
     }
   ]
 
   onNewRequi(requisito: Req):void {
-    this.requisites.push(requisito);
+
+    const newRequi: Req = {
+      id: uuid(),
+      ...requisito
+    }
+    
+    this.requisites.push(newRequi);
   }
 
   onDeleteReqMain(index: number): void {
